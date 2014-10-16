9fad08c7a7fea134371dddeaafc97ee3686df80a
Steve Tooke <steve@boxjump.co.uk>
2014-10-16 17:34:42 +0100

    Add spec for 5 book set

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -31,7 +31,9 @@ describe "pricing sets of harry potter books" do
   let(:checkout) { Checkout.new }
   let(:book1) { OpenStruct.new name: 'book1', price: 8 }
   let(:book2) { OpenStruct.new name: 'book2', price: 8 }
+  let(:book3) { OpenStruct.new name: 'book3', price: 8 }
   let(:book4) { OpenStruct.new name: 'book4', price: 8 }
+  let(:book5) { OpenStruct.new name: 'book5', price: 8 }
 
   it "provides no discount for a single book" do
     checkout.scan book1
@@ -66,13 +68,21 @@ describe "pricing sets of harry potter books" do
   end
 
   it "discounts a set of four books by 20%" do
-    book3 = OpenStruct.new name: 'book3', price: 8
-
     checkout.scan book1
     checkout.scan book2
     checkout.scan book3
     checkout.scan book4
 
     expect( checkout.total ).to eq 32 * 0.80
+  end
+
+  it "discount a set of five books by 25%" do
+    checkout.scan book1
+    checkout.scan book2
+    checkout.scan book3
+    checkout.scan book4
+    checkout.scan book5
+
+    expect( checkout.total ).to eq 40 * 0.75
   end
 end
