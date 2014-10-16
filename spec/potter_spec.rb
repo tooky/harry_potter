0c42ccb58c0832fa02e46195e1890c8039826502
Steve Tooke <steve@boxjump.co.uk>
2014-10-16 17:29:57 +0100

    Add spec for 3 book set

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -9,6 +9,8 @@ class Checkout
     discount = case books.uniq.count
                when 2
                  5
+               when 3
+                 10
                else
                  0
                end
@@ -26,6 +28,7 @@ end
 describe "pricing sets of harry potter books" do
   let(:checkout) { Checkout.new }
   let(:book1) { OpenStruct.new name: 'book1', price: 8 }
+  let(:book4) { OpenStruct.new name: 'book4', price: 8 }
 
   it "provides no discount for a single book" do
     checkout.scan book1
@@ -42,11 +45,20 @@ describe "pricing sets of harry potter books" do
   end
 
   it "discounts a set of two books by 5%" do
-    book4 = OpenStruct.new name: 'book4', price: 8
 
     checkout.scan book1
     checkout.scan book4
 
     expect( checkout.total ).to eq 16 * 0.95
+  end
+
+  it "discounts a set of three books by 10%" do
+    book2 = OpenStruct.new name: 'book2', price: 8
+
+    checkout.scan book1
+    checkout.scan book2
+    checkout.scan book4
+
+    expect( checkout.total ).to eq 24 * 0.90
   end
 end
