81f1ad19ce7c396d9a65c4b94c25a0fc44855a0a
Steve Tooke <steve@boxjump.co.uk>
2014-10-16 17:09:28 +0100

    Check multiples of a single book

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -23,4 +23,14 @@ describe "pricing sets of harry potter books" do
 
     expect( checkout.total ).to eq 8
   end
+
+  it "does not discount multiples of one book" do
+    book1 = OpenStruct.new name: 'book1', price: 8
+    checkout = Checkout.new
+    checkout.scan book1
+    checkout.scan book1
+    checkout.scan book1
+
+    expect( checkout.total ).to eq 24
+  end
 end
