15a8580d9edbc4fc3c34ba050bbac2c3abf2dc06
Steve Tooke <steve@boxjump.co.uk>
2014-10-16 17:06:14 +0100

    Make the test "compile"

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -1,9 +1,21 @@
+require 'ostruct'
+
+class Checkout
+  def scan book
+
+  end
+
+  def total
+
+  end
+end
+
 describe "pricing sets of harry potter books" do
   it "provides no discount for a single book" do
-    book1 = OpenStruct.new(name: 'book1', price: 8)
+    book1 = OpenStruct.new name: 'book1', price: 8
     checkout = Checkout.new
     checkout.scan book1
 
-    expect( checkout.total ).to eq( 8 )
+    expect( checkout.total ).to eq 8
   end
 end
