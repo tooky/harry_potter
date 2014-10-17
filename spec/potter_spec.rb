6213be39ed35dbfe4a62666072cb68fcdea32764
Steve Tooke <steve@boxjump.co.uk>
2014-10-17 12:40:15 +0100

    Add a helper method to make the specs a bit clearer

--- spec/potter_spec.rb
+++ spec/potter_spec.rb
@@ -9,74 +9,58 @@ describe "pricing sets of harry potter books" do
   let(:book4) { OpenStruct.new name: 'book4', price: 8 }
   let(:book5) { OpenStruct.new name: 'book5', price: 8 }
 
+  def scan(*books)
+    books.each { |b| checkout.scan b }
+  end
+
   it "provides no discount for a single book" do
-    checkout.scan book1
+    scan book1
 
     expect( checkout.total ).to eq 8
   end
 
   it "does not discount multiples of one book" do
-    checkout.scan book1
-    checkout.scan book1
-    checkout.scan book1
+    scan book1, book1, book1
 
     expect( checkout.total ).to eq 24
   end
 
   it "discounts a set of two books by 5%" do
-
-    checkout.scan book1
-    checkout.scan book4
+    scan book1, book4
 
     expect( checkout.total ).to eq 16 * 0.95
   end
 
   it "discounts a set of three books by 10%" do
-    book2 = OpenStruct.new name: 'book2', price: 8
-
-    checkout.scan book1
-    checkout.scan book2
-    checkout.scan book4
+    scan book1, book2, book4
 
     expect( checkout.total ).to eq 24 * 0.90
   end
 
   it "discounts a set of four books by 20%" do
-    checkout.scan book1
-    checkout.scan book2
-    checkout.scan book3
-    checkout.scan book4
+    scan book1, book2, book3, book4
 
     expect( checkout.total ).to eq 32 * 0.80
   end
 
   it "discount a set of five books by 25%" do
-    checkout.scan book1
-    checkout.scan book2
-    checkout.scan book3
-    checkout.scan book4
-    checkout.scan book5
+    scan book1, book2, book3, book4, book5
 
     expect( checkout.total ).to eq 40 * 0.75
   end
 
   it "does not discount books outside of the set" do
-    checkout.scan book1
-    checkout.scan book1
-    checkout.scan book2
+    scan book1, book1, book2
 
     expect( checkout.total ).to eq 8 + 16 * 0.95
   end
 
   it "handles finding the best discount" do
-    checkout.scan book1
-    checkout.scan book1
-    checkout.scan book2
-    checkout.scan book2
-    checkout.scan book3
-    checkout.scan book3
-    checkout.scan book4
-    checkout.scan book5
+    scan book1, book1
+    scan book2, book2
+    scan book3, book3
+    scan book4
+    scan book5
 
     expect( checkout.total ).to eq( 32 * 0.8 + 32 * 0.8 )
   end
